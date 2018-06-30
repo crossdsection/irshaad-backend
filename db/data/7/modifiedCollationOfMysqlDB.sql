a304fb62cc628cf3d2b599b9526cffce61ad43f5
crossdsection <chirkut.kr@gmail.com>
2018-06-30 18:49:04 +0530

    Bug Fix old Null acceptance was removed

--- db/data/7/modifiedCollationOfMysqlDB.sql
+++ db/data/7/modifiedCollationOfMysqlDB.sql
@@ -18,17 +18,17 @@ ALTER TABLE `wv_votes_reviews` CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicod
 
 ALTER TABLE `wv_user` CHANGE `firstname` `firstname` VARCHAR(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
 ALTER TABLE `wv_user` CHANGE `lastname` `lastname` VARCHAR(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
-ALTER TABLE `wv_user` CHANGE `gender` `gender` VARCHAR(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
+ALTER TABLE `wv_user` CHANGE `gender` `gender` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
 ALTER TABLE `wv_user` CHANGE `email` `email` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
 ALTER TABLE `wv_user` CHANGE `password` `password` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
-ALTER TABLE `wv_user` CHANGE `phone` `phone` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
-ALTER TABLE `wv_user` CHANGE `address` `address` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
-ALTER TABLE `wv_user` CHANGE `latitude` `latitude` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
-ALTER TABLE `wv_user` CHANGE `longitude` `longitude` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
-ALTER TABLE `wv_user` CHANGE `profilepic` `profilepic` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
+ALTER TABLE `wv_user` CHANGE `phone` `phone` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `wv_user` CHANGE `address` `address` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `wv_user` CHANGE `latitude` `latitude` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `wv_user` CHANGE `longitude` `longitude` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `wv_user` CHANGE `profilepic` `profilepic` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
 ALTER TABLE `wv_user` CHANGE `access_role_ids` `access_role_ids` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
-ALTER TABLE `wv_user` CHANGE `rwa_name` `rwa_name` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
-ALTER TABLE `wv_user` CHANGE `designation` `designation` VARCHAR(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
-ALTER TABLE `wv_user` CHANGE `certificate` `certificate` VARCHAR(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL;
+ALTER TABLE `wv_user` CHANGE `rwa_name` `rwa_name` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `wv_user` CHANGE `designation` `designation` VARCHAR(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
+ALTER TABLE `wv_user` CHANGE `certificate` `certificate` VARCHAR(128) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;
 
 ALTER TABLE `wv_user` CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
